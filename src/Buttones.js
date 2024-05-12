3e5b1cacbe34571dc0d5c39eacbccf0a44e40d97
Saneep dalal <171ankit.2020@gmail.com>
2024-05-12 12:22:56 +0530

    add

---
+++ src/Buttones.js
@@ -0,0 +1,23 @@
+import "./Buttones.css"
+export default function Buttons(){
+    return (
+        <div id="buttonees">
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>+</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>-</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>*</button>
+            <button>C</button>
+            <button>0</button>
+            <button>=</button>
+            <button>/</button>
+        </div>
+    )
+}
---
+++ src/Calculator.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import "./Buttones.css";
+
+export default function Calculator() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+
+  const handleInput = (value) => {
+    setInput(input + value);
+  };
+
+  const handleClear = () => {
+    setInput("");
+    setOutput("");
+  };
+
+  const handleOperator = (value) => {
+    if (input.slice(-1) === value || input.slice(-1) === "=") {
+      setInput(input.slice(0, -1));
+    }
+    setInput(input + value);
+  };
+
+  const handleEqual = () => {
+    try {
+      const result =eval(input.replace(/x/g, "*").replace(/÷/g, "/"));
+      setOutput(result);
+    } catch (error) {
+      setOutput("Error");
+    }
+  };
+
+  return (
+    <>
+      <div className="calculator">
+        <h1>React Calculator</h1>
+        <input type="text" value={input} readOnly />
+        <div className="resultinput">
+          {output && <div className="result">{output}</div>}
+        </div>
+        <div className="buttons">
+          <button onClick={() => handleInput("7")}>7</button>
+          <button onClick={() => handleInput("8")}>8</button>
+          <button onClick={() => handleInput("9")}>9</button>
+          <button onClick={() => handleOperator("+")}>+</button>
+          <button onClick={() => handleInput("4")}>4</button>
+          <button onClick={() => handleInput("5")}>5</button>
+          <button onClick={() => handleInput("6")}>6</button>
+          <button onClick={() => handleOperator("-")}>-</button>
+          <button onClick={() => handleInput("1")}>1</button>
+          <button onClick={() => handleInput("2")}>2</button>
+          <button onClick={() => handleInput("3")}>3</button>
+          <button onClick={() => handleOperator("*")}>*</button>
+          <button onClick={handleClear}>C</button>
+          <button onClick={() => handleInput("0")}>0</button>
+          <button onClick={handleEqual}>=</button>
+          <button onClick={() => handleOperator("/")}>/</button>
+        </div>
+      </div>
+    </>
+  );
+}
